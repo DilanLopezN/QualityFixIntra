b8d0912d88f3790a7f03cb9d3d998d4ac762098e
Bruna Lopez <brunalopez.421@gmail.com>
2023-05-30 10:41:02 -0300

    base project

---
+++ src/Router.tsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from "react-router-dom";
+import { SignIn } from "./pages/Signin";
+
+export function Router() {
+  return (
+    <Routes>
+        <Route path="/" element={<SignIn/>} /> 
+      </Routes>
+  )
+  
+}
